ed3dda3e45b5a1a661b50d9656dda0e4cf169756
RobertUlloa <roanuc8@gmail.com>
2024-10-01 16:39:44 -0500

    react structure finished, but i need to debug it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
+import Home from './pages/Home'; 
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
