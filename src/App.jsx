1ef77b22d6e023e5df04d5b0ecf93eb8e6be81c1
RobertUlloa <roanuc8@gmail.com>
2024-10-03 14:44:08 -0500

    app is working locally, i need to apply css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home'; 
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/componets/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Portfolio</Link>
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+          <li>
+            <Link to="/portfolio" className="nav-link">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
